70820ed3d68d114b329646d82169cc286c81a467
h17ao <903527820@qq.com>
2025-09-10 00:13:42 +0800

    1

--- run_32b_with_iteration.sh
+++ run_32b_with_iteration.sh
@@ -10,11 +10,7 @@ python main.py -opt Option/Method/Dalk_Hao.yaml -dataset_name 2wiki
 python main.py -opt Option/Method/GR_Hao.yaml -dataset_name 2wiki
 python main.py -opt Option/Method/KGP_Hao.yaml -dataset_name 2wiki
 
-python main.py -opt Option/Method/HippoRAG_Hao.yaml -dataset_name musique
-python main.py -opt Option/Method/LightRAG_Hao.yaml -dataset_name musique
-python main.py -opt Option/Method/Dalk_Hao.yaml -dataset_name musique
-python main.py -opt Option/Method/GR_Hao.yaml -dataset_name musique
-python main.py -opt Option/Method/KGP_Hao.yaml -dataset_name musique
+
 
 # python main.py -opt Option/Method/ToG_Hao.yaml -dataset_name hotpotqa
 # python main.py -opt Option/Method/ToG_Hao.yaml -dataset_name wiki
